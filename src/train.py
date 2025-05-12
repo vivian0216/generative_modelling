f08358ee87eea3343719d2981f71ba3056c3153d
Emiel Witting <e.a.witting@student.tudelft.nl>
2025-05-12 13:45:56 +0200

    add logging

--- src/train.py
+++ src/train.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 import numpy as np
 from tqdm import tqdm
 import random 
+import wandb
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu'# 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def train(model: nn.Module,
           dataset: torch.utils.data.Dataset,
@@ -27,7 +28,12 @@ def train(model: nn.Module,
     model.to(device)
 
     for e in range(epochs):
-        for x, y in tqdm(dataloader, unit='batch', desc=f'Epoch {e+1}'):
+        clf_losses = []
+        gen_losses = []
+        combined_losses = []
+        accs = []
+        pbar = tqdm(dataloader, unit='batch', desc=f'Epoch {e+1}')
+        for x, y in pbar:
             x = x.to(device)
             y = y.to(device)
             
@@ -44,7 +50,7 @@ def train(model: nn.Module,
 
             # perform SGLD
             for t in range(steps):
-                xt = torch.tensor(xt.clone().detach(), requires_grad=True).to(device)
+                xt = xt.clone().detach().requires_grad_(True).to(device)
                 xt.retain_grad()
                 xt_logits = model(xt)
                 logsumexp = torch.logsumexp(xt_logits, dim=-1)
@@ -60,7 +66,7 @@ def train(model: nn.Module,
             # get generation loss
             xt_logits = model(xt)
             loss_gen = torch.logsumexp(x_logits, dim=-1) - torch.logsumexp(xt_logits, dim=-1)
-            loss =  loss_clf + loss_gen
+            loss = loss_clf #+ loss_gen
 
             # do model step using optimizer
             optimizer.zero_grad()
@@ -71,6 +77,27 @@ def train(model: nn.Module,
             buffer.append(xt)
             if len(buffer) > buffer_size:
                 buffer = buffer[1:]
+
+            # add metrics
+            clf_losses.append(loss_clf.item())
+            gen_losses.append(loss_gen.item())
+            combined_losses.append(loss.item())
+            accs.append(torch.argmax(x_logits, dim=-1) == y)
+            pbar.set_postfix({
+                'clf_loss': np.mean(clf_losses),
+                'gen_loss': np.mean(gen_losses),
+                'loss': np.mean(combined_losses),
+                'acc': np.mean(accs)
+            })
+
+        # log metrics
+        wandb.log({
+            'Training/loss_clf': np.mean(clf_losses),
+            'Training/loss_gen': np.mean(gen_losses),
+            'Training/loss': np.mean(combined_losses),
+            'epoch': e
+        })
+
 
         
 class Model(nn.Module):
@@ -86,9 +113,10 @@ if __name__ == "__main__":
     from torchvision.datasets import MNIST
     from torchvision import transforms
 
-    dataset = MNIST(root='../data', train=True, download=True, transform=transforms.Compose([
+    # Get only a fraction of the dataset
+    full_dataset = MNIST(root='../data', train=True, download=True, transform=transforms.Compose([
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: torch.flatten(x))
+        transforms.Lambda(lambda x: torch.flatten(x) * 2 - 1)
     ]))
 
     in_dim = 28 * 28
@@ -96,17 +124,28 @@ if __name__ == "__main__":
 
     model = Model(in_dim, out_dim)
 
+    cfg = dict(
+        step_size = 1, 
+        noise = 0.01, 
+        buffer_size = 10000,
+        steps = 20,
+        reinit_freq = 0.05,
+        epochs = 10,
+        learning_rate = 1e-3,
+        learning_rate_decay = None,
+        learning_rate_epochs = None,
+        train_fraction = 0.05,
+    )
+        
+    # Use 10% of the dataset
+    subset_size = int(len(full_dataset) * cfg.pop('train_fraction'))
+    indices = np.random.RandomState(seed=42).permutation(len(full_dataset))[:subset_size]
+    dataset = torch.utils.data.Subset(full_dataset, indices)
+    wandb.init(project="generative-modelling", config=cfg)
+
     train(model = model,
           dataset = dataset,
-          step_size = 1, 
-          noise = 0.01, 
-          buffer_size = 10000,
-          steps = 20,
-          reinit_freq = 0.05,
-          epochs = 10,
-          learning_rate = 1e-2,
-          learning_rate_decay = None,
-          learning_rate_epochs = None)
+          **cfg)
     
 
         
