4989fa8c4958cfba9ee03fafd5f6f2dbcb9787f7
Emiel <emiel.witting@gmail.com>
2025-05-21 21:40:23 +0200

    disable dropout

--- src/train.py
+++ src/train.py
@@ -190,14 +190,14 @@ class CNN(nn.Module):
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = F.dropout(x, p=0.5, training=self.training)
+        # x = F.dropout(x, p=0.5, training=self.training)
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.dropout(x, p=0.5, training=self.training)
+        # x = F.dropout(x, p=0.5, training=self.training)
         x = F.relu(F.max_pool2d(self.conv3(x),2))
-        x = F.dropout(x, p=0.5, training=self.training)
+        # x = F.dropout(x, p=0.5, training=self.training)
         x = x.view(-1,3*3*64 )
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        # x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return x
 
